e7981d36865a4561b8e356a9bd2480e480f4081e
janaaaac <cjdissanayake@students.nsbm.ac.lk>
2024-03-12 18:46:27 +0530

    login & register page added

---
+++ lib/screens/authenticate/register.dart
@@ -0,0 +1,166 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:cookit/components/my_button.dart';
+import 'package:cookit/components/my_textfield.dart';
+import 'package:cookit/components/square_tile.dart';
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+  final Function()? onTap;
+  const RegisterPage({super.key, required this.onTap});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  //text editing controllers
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final confirmpasswordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 20),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(
+                        width:
+                            10), // Adjust the spacing between the icon and the image
+                    Image.asset(
+                      'lib/images/Subject 16.png',
+                      width: 200, // Adjust the width of the image as needed
+                      height: 200, // Adjust the height of the image as needed
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 25),
+
+                //Let\'s create an account for you!
+
+                Text(
+                  'Let\'s create an account for you!',
+                  style: TextStyle(
+                    color: Colors.grey[700],
+                    fontSize: 16,
+                  ),
+                ),
+                const SizedBox(height: 25),
+                //Name textfield
+                MytextField(
+                    controller: emailController,
+                    hintText: 'Full name',
+                    obscureText: false),
+                const SizedBox(height: 10),
+
+                //email textfield
+                MytextField(
+                    controller: emailController,
+                    hintText: 'Email',
+                    obscureText: false),
+                const SizedBox(height: 10),
+
+                //password textfield
+                MytextField(
+                    controller: passwordController,
+                    hintText: 'Password',
+                    obscureText: true),
+                const SizedBox(height: 10),
+
+                //Confirm password textfield
+                MytextField(
+                    controller: confirmpasswordController,
+                    hintText: 'Confirm Password',
+                    obscureText: true),
+
+                const SizedBox(height: 25),
+                //sign in button
+                MyButton(
+                  text: "Sign Up",
+                  onTap: null,
+                ),
+                const SizedBox(height: 50),
+
+                //or continue with
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          thickness: 0.5,
+                          color: Colors.grey[400],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Text(
+                          'Or Continue With',
+                          style: TextStyle(color: Colors.grey[700]),
+                        ),
+                      ),
+                      Expanded(
+                        child: Divider(
+                          thickness: 0.5,
+                          color: Colors.grey[400],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 50),
+                //google or apple id
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    //google button
+
+                    SquareTile(imagepath: 'lib/images/google.png'),
+                    SizedBox(width: 25),
+
+                    //apple button
+                    SquareTile(imagepath: 'lib/images/apple.png'),
+                  ],
+                ),
+
+                const SizedBox(height: 50),
+                //not a member? register now
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Already have an account?',
+                      style: TextStyle(color: Colors.grey[700]),
+                    ),
+                    const SizedBox(width: 4),
+                    GestureDetector(
+                      onTap: widget.onTap,
+                      child: const Text(
+                        'Login now',
+                        style: TextStyle(
+                          color: Color(0xFF86BF3E),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
