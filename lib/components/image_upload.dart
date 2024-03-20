c473d45a4e9ad91280a64b500c4c86eaaa1bd878
Ishuwaraa <115783906+Ishuwaraa@users.noreply.github.com>
2024-03-20 19:50:10 +0530

    Image upload function added

---
+++ lib/components/image_upload.dart
@@ -0,0 +1,58 @@
+import 'dart:io';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ImageUpload extends StatefulWidget {
+
+  final Function(String) onImageUrlChange;
+
+  // const ImageUpload({super.key});
+  const ImageUpload({required this.onImageUrlChange, Key? key}) : super(key: key);
+
+  @override
+  State<ImageUpload> createState() => _ImageUploadState();
+}
+
+class _ImageUploadState extends State<ImageUpload> {
+  String imageUrl = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () async {
+        ImagePicker imagePicker = ImagePicker();
+        XFile? file = await imagePicker.pickImage(source: ImageSource.gallery);
+        print(file?.path);        
+
+        if(file == null) return;
+
+        //reference to firebase storage
+        Reference refRoot = FirebaseStorage.instance.ref();
+        Reference refDirImages = refRoot.child('users');  //ref to sub folder
+
+        //creating a unique name for the image
+        // String uniqueFileName = DateTime.now().microsecondsSinceEpoch.toString();
+        String formattedDateTime = DateTime.now().toString(); 
+        String uniqueFileName = formattedDateTime.replaceAll(RegExp(r'[^0-9]'), ''); // Removing non-numeric characters
+
+        //ref for the image to be stored
+        Reference refImageToUpload = refDirImages.child(uniqueFileName);  //'${file?.path}'
+
+        try{
+          //storing the file
+          await refImageToUpload.putFile(File(file.path));
+
+          //download url
+          imageUrl = await refImageToUpload.getDownloadURL();
+          print(imageUrl);
+          widget.onImageUrlChange(imageUrl);
+
+        }catch(e) {
+          print(e.toString());
+        }
+      },
+      child: const Text('Upload image'),
+    );
+  }
+}
--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -1,6 +1,11 @@
+import 'package:cookit/components/image_upload.dart';
+import 'package:cookit/components/loading.dart';
+import 'package:cookit/models/user_model.dart';
 import 'package:cookit/screens/profile/profile_settings.dart';
 import 'package:cookit/services/auth.dart';
+import 'package:cookit/services/database.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Profile extends StatefulWidget {
   const Profile({super.key});
@@ -12,28 +17,69 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
 
   final AuthService _auth = AuthService();
+  String imageUrl = '';
+
+  void updateImageUrl(String newUrl) {
+    setState(() {
+      imageUrl = newUrl; 
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('COOKIT'),
-      ),
-      body: Column(
-        children: [
-          const Text('Hello world'),
-          ElevatedButton(
-            onPressed: () async {
-              await _auth.signOutUser();
-            }, 
-            child: const Text('Log out')),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) => const ProfileSettings()));
-            },
-            child: const Text('Settings page'))
-        ],
-      ),
+
+    final user = Provider.of<UserModel>(context);
+
+    return StreamBuilder<UserData>(
+      stream: DatabaseService(userId: user.userId).userData,
+      builder: (context, AsyncSnapshot<UserData> snapshot) {
+        if(snapshot.hasError){
+          return Center(child: Text('Sorry, an error occured. ${snapshot.error}'),);
+        }
+        if(snapshot.hasData){
+          UserData userData = snapshot.data!;
+
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('COOKIT'),
+            ),
+            body: Column(
+              children: [
+                Text(userData.userId),
+                Text(userData.name),
+                Text(userData.email),
+                Text(userData.profilePicUrl),
+                Text('imageUrl: $imageUrl'),
+                const SizedBox(height: 20.0,),
+                Container(
+                  width: 200.0,
+                  height: 200.0,
+                  child: (imageUrl.isNotEmpty)? Image.network(imageUrl) : const Text('no image uploaded'),
+                ),
+                const SizedBox(height: 20.0,),
+                ImageUpload(onImageUrlChange: (newUrl) {
+                  updateImageUrl(newUrl);
+                }),
+                const SizedBox(height: 20.0,),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => const ProfileSettings()));
+                  },
+                  child: const Text('Settings page')
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    await _auth.signOutUser();
+                  }, 
+                  child: const Text('Log out')),
+              ],
+            ),
+          );
+        }else{
+          return const Loading();
+        }
+      }
     );
+    
   }
 }
