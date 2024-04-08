e6f191ac2c17a27ac5a7054fb4cb8f5cbd01a1ed
janaaaa <cjdissanayake@students.nsbm.ac.lk>
2024-04-08 20:22:43 +0530

    forgot password , recipe details page,profile settings, add recipe and user favoriteupdated

--- lib/screens/user_favorite.dart
+++ lib/screens/user_favorite.dart
@@ -1,23 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:cookit/components/appbar_title.dart';
 import 'package:cookit/components/card.dart';
 import 'package:cookit/components/loading.dart';
 import 'package:cookit/models/recipe_model.dart';
 import 'package:cookit/models/user_model.dart';
 import 'package:cookit/services/database.dart';
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class UserFavourites extends StatefulWidget {
-  const UserFavourites({super.key});
+  const UserFavourites({Key? key}) : super(key: key);
 
   @override
   State<UserFavourites> createState() => _UserFavouritesState();
 }
 
 class _UserFavouritesState extends State<UserFavourites> {
-  bool loading = false;
-
-  Future<List<Recipe>> getFavouriteRecip(String userId) async {
+  Future<List<Recipe>> getFavouriteRecipes(String userId) async {
     List<Recipe> favouriteRecipes =
         await DatabaseService.getFavouriteRecipes(userId);
     return favouriteRecipes;
@@ -28,67 +26,57 @@ class _UserFavouritesState extends State<UserFavourites> {
     final user = Provider.of<UserModel>(context);
 
     return FutureBuilder(
-        future: getFavouriteRecip(user.userId),
-        builder: (context, AsyncSnapshot<List<Recipe>> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Loading();
-          }
+      future: getFavouriteRecipes(user.userId),
+      builder: (context, AsyncSnapshot<List<Recipe>> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Loading();
+        }
 
-          if (snapshot.hasData) {
-            List<Recipe> favouriteRecipes = snapshot.data ?? [];
+        if (snapshot.hasData) {
+          List<Recipe> favouriteRecipes = snapshot.data ?? [];
 
-            return Scaffold(
-              appBar: AppBar(
-                title: const AppbarTitle(title: 'Favourites'),
-              ),
-              body: Container(
-                padding: const EdgeInsets.all(16.0),
-                child: Expanded(
-                  child: ListView.builder(
-                      itemCount: (favouriteRecipes.isEmpty)
-                          ? 1
-                          : favouriteRecipes.length,
-                      itemBuilder: (_, index) {
-                        if (favouriteRecipes.isNotEmpty) {
-                          // return Column(
-                          //   children: [
-                          //     Text('recipe id: ${userData.favourites![index]}'),
-                          //   ],
-                          // );
-                          return Column(children: [
-                            FoodCard(favouriteRecipes[index], type: 'fav'),
-                            const SizedBox(
-                              height: 30.0,
-                            ),
-                          ]);
-                        } else {
-                          return const Center(
-                            child: Padding(
-                              padding: const EdgeInsets.all(20.0),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    'Add your favourite recipes here for easy access',
-                                    style: TextStyle(fontSize: 18.0),
-                                    textAlign: TextAlign.center,
-                                  ),
-                                ],
-                              ),
-                            ),
-                          );
-                        }
-                      }),
-                ),
-              ),
-            );
-          } else {
-            return const Column(
-              children: [Text('no data')],
-            );
-          }
-        });
+          return Scaffold(
+            appBar: AppBar(
+              title: const AppbarTitle(title: 'Favourites'),
+            ),
+            body: favouriteRecipes.isEmpty
+                ? const Center(
+                    child: Padding(
+                      padding: EdgeInsets.all(20.0),
+                      child: Text(
+                        'Add your favourite recipes here for easy access',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.grey,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  )
+                : ListView.builder(
+                    itemCount: favouriteRecipes.length,
+                    itemBuilder: (_, index) {
+                      return Column(
+                        children: [
+                          FoodCard(favouriteRecipes[index], type: 'fav'),
+                          const SizedBox(height: 30.0),
+                        ],
+                      );
+                    },
+                  ),
+          );
+        } else {
+          return Scaffold(
+            appBar: AppBar(
+              title: const AppbarTitle(title: 'Favourites'),
+            ),
+            body: const Center(
+              child: Text('No data'),
+            ),
+          );
+        }
+      },
+    );
 
     // return StreamBuilder(
     //   stream: DatabaseService(userId: user.userId).userData,
